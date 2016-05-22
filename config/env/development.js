3a69c6a40c28bf03218c051293b52385720959ff
raphaelmayer <mayer.raphael@gmail.com>
2016-05-22 21:21:09 +0200

    change profile

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    db: 'mongodb://node:Start123@ds011452.mlab.com:11452/heroku_2ncs5zdw',
     debug: true,
     logging: {
         format: 'tiny'
--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
+  db: 'mongodb://node:Start123@ds011452.mlab.com:11452/heroku_2ncs5zdw',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
